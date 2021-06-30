4dcf4316923fa505cf039fe62f9c31960ea51f20
Martina <martinaescorial9@gmail.com>
2021-06-30 21:49:55 +0200

    Add project files.

---
+++ UD5E16/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace UD5E16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 2, 1 };
+            int length = array.Length;
+            bool mirror = true;
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (mirror)
+                {
+                    int start = array[index];
+                    int end = array[--length];
+                    if (length < index)
+                    {
+                        break;
+                    }
+                    if (start != end)
+                    {
+                        mirror = false;
+                    }
+                }
+            }
+            if (mirror)
+            {
+                Console.WriteLine("Es capicúa");
+            }
+            else
+            {
+                Console.WriteLine("No es capicúa");
+            }
+        }
+    }
+}
